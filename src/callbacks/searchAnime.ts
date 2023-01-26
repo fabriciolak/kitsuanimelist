32454d1e35f279f6728ddca859573ce6a66410ad
Fabrício <fabriciolak12@gmail.com>
2023-01-26 18:52:03 -0300

    :sparkles: feat: add callback function to send the anime trailer

--- src/callbacks/searchAnime.ts
+++ src/callbacks/searchAnime.ts
@@ -1,9 +1,25 @@
 import TelegramBot from "node-telegram-bot-api";
 
+interface watchAnimeTrailerType {
+  youtubeVideoId: string
+}
+
 export function searchAnimeCallback(bot: TelegramBot, callbackQuery: TelegramBot.CallbackQuery) {
   const anime = callbackQuery.message?.text
   
-  bot.answerCallbackQuery(callbackQuery.id)
+  if (callbackQuery.data === 'search_anime') {
+    bot.answerCallbackQuery(callbackQuery.id)
+    bot.sendMessage(callbackQuery.message?.chat.id!, 'oi')
+  }
   
-  bot.sendMessage(callbackQuery.message?.chat.id!, 'oi')
+}
+
+export function watchAnimeTrailer(bot: TelegramBot, options: watchAnimeTrailerType, callbackQuery: TelegramBot.CallbackQuery) {
+  const message = `Confira o trailer desse anime incrível aqui:\n\nhttps://www.youtube.com/watch?v=${options.youtubeVideoId}`
+
+  if (callbackQuery.data === 'trailer_anime') {
+    bot.answerCallbackQuery(callbackQuery.id)
+    bot.sendMessage(callbackQuery.message?.chat.id!, message)
+  }
+
 }
