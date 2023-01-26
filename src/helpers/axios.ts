acba66c8e5844e20445747b4dafc7d1594e336d0
Fabrício <fabriciolak12@gmail.com>
2023-01-26 20:20:50 -0300

    :sparkles: feat: create axios config

---
+++ src/helpers/axios.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+const baseURL = 'https://kitsu.io/api/oauth';
+
+const data = axios({
+  method: 'post',
+  url: `${baseURL}/token`,
+  auth: {
+    username: clientId!,
+    password: clientSecret!
+  },
+  data: {
+    grant_type: 'password',
+    username: 'fabricio',
+    password: process.env.KITSU_ACCOUNT_PASSWORD
+  },
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${data.then(response => response.data.access_token)}`
+  }
+})
